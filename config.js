37a8ea3d473226e64b72fbe6186aff99bca416b1
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2022-11-16 15:20:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 }
 global.packname = data.namabot
 global.author = data.owner
-global.wm = 'DevXyZ'
+global.wm = 'Niluhchan'
 
 global.multiplier = 100 // The higher, The harder levelup
 
